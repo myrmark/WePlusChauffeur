25de044270e65898247c1a4215606fa18bc160f9
myrmark <filip.malmberg@outlook.com>
2022-05-10 20:55:15 +0200

    Update mainscript.py

--- mainscript.py
+++ mainscript.py
@@ -20,54 +20,56 @@ from selenium.webdriver.support.ui import Select
 emoji = u'\U0001F44D'
 
 email = input("Enter your email: ")
-password = getpass()
+password = getpass.getpass()
 
 driver = webdriver.Firefox()
 driver.get('https://weplusapp.com')
 driver.implicitly_wait(20)
 logincheck = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[1]/div[2]/div/form/div[3]/a[1]"))).text
 if logincheck:
-    email = driver.find_element_by_id("email")
-    password = driver.find_element_by_id("password")
-    email.send_keys(email)
-    password.send_keys(password)
+    emailfield = driver.find_element(by=By.ID, value="email")
+    passwordfield = driver.find_element(by=By.ID, value="password")
+    emailfield.send_keys(email)
+    time.sleep(1)
+    passwordfield.send_keys(password)
     loginbutton = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[2]/div/form/div[3]/input").click()
 driver.implicitly_wait(20)
-time.sleep(3)
+time.sleep(1)
 companybutton = driver.find_element(by=By.XPATH, value="/html/body/ul[1]/li[2]/a")
 companybutton.click()
 driver.implicitly_wait(20)
-time.sleep(3)
+time.sleep(1)
 select = Select(driver.find_element(by=By.XPATH, value="/html/body/div[2]/div[4]/div/div/form/div[1]/div[1]/div/select"))
 select.select_by_value("created-at")
 driver.implicitly_wait(20)
-time.sleep(3)
+time.sleep(1)
 selectall = Select(driver.find_element(by=By.XPATH, value="/html/body/div[2]/div[4]/div/div/form/div[1]/div[2]/div/select"))
 selectall.select_by_value("all")
 driver.implicitly_wait(20)
-time.sleep(3)
+time.sleep(1)
 i = 1
 while True:
+    print(i)
     if i == 7 or i == 13 or i == 19 or i == 25 or i == 31 or i == 37 or i == 43 or i == 49 or i == 55 or i == 61:
         morebutton = driver.find_element(by=By.XPATH, value="/html/body/div[2]/div[4]/div/ul/li[{}]/a".format(i)).click()
         driver.implicitly_wait(20)
-    time.sleep(3)
+    time.sleep(1)
     postpath = "/html/body/div[2]/div[4]/div/ul/li[{}]".format(i)
     post = driver.find_element(by=By.XPATH, value=postpath).click()
     driver.implicitly_wait(20)
-    time.sleep(3)
+    time.sleep(1)
+    namefield = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div[2]/div/div/ul/li/div/div[1]/div/h3/strong"))).text
+    name = namefield.split(' ')[0]
     try:
         likebutton = driver.find_element(by=By.XPATH, value="/html/body/div[2]/div[2]/div/div/ul/li/div/ul/li/a")
     except Exception:
-        continue
-    if not likebutton:
+        print("Likebutton not found!")
+        input("Script finished. Press any button to exit.")
         driver.close()
         break
-    else:
-        likebutton.click()
-    time.sleep(3)
-    namefield = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div[2]/div/div/ul/li/div/div[1]/div/h3/strong"))).text
-    name = namefield.split(' ')[0]
+    print("Clicking likebutton on {}'s post".format(name))
+    likebutton.click()
+    time.sleep(1)
     commentlist = [
                     "Good job {}! {}{}".format(name,emoji,emoji),
                     "Keep up the good work {}! {}{}".format(name,emoji,emoji),
@@ -85,11 +87,13 @@ while True:
                     "All aboard the {}-fitnesstrain! Choo choo! {}{}".format(name,emoji,emoji)
                     ]
     comment = random.choice(commentlist)
+    print("Commenting on {}'s post".format(name))
     commentfield = driver.find_element(by=By.XPATH, value="""//*[@id="comment_body"]""")
     commentfield.send_keys(comment)
-    time.sleep(3)
+    print(comment)
+    time.sleep(1)
     commentfield.send_keys(Keys.RETURN)
-    time.sleep(3)
+    time.sleep(1)
     xbutton = driver.find_element(by=By.XPATH, value="/html/body/div[2]/div[2]/div/a").click()
-    time.sleep(3)
+    time.sleep(1)
     i += 1
